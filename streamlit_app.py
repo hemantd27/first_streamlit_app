30c4a5100656445a54769ed877b3d285e641bc78
Hemant Ramesh Dange <104827347+hemantd27@users.noreply.github.com>
2022-05-03 16:39:33 +0530

    Update streamlit_app.py
    added new import as pandas
    added code to read from a file "fruit_macros.txt" using pandas library.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
+import pandas
 
-streamlit.title('My Mom\'ss Healthy Dinner')
+streamlit.title('My Mom\'s Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal ')
@@ -9,3 +10,5 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
