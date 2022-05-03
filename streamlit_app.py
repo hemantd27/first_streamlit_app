61aabae3aafc2bed589c5a8509544a4252c621a9
Hemant Ramesh Dange <104827347+hemantd27@users.noreply.github.com>
2022-05-03 16:21:36 +0530

    Update streamlit_app.py
    spelling correction done.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'ss Healthy Dinner')
 
-streamlit.header('Brekfast Menu')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal ')
 streamlit.text('Kale,Spinach & rocket smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
