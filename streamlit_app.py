273c1760ecd5a67f8b4d6f97c9a6c48bb1eb2bc7
Hemant Ramesh Dange <104827347+hemantd27@users.noreply.github.com>
2022-05-03 16:18:29 +0530

    Update streamlit_app.py
    added more lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
-streamlit.title('My Parents Healthy Dinner')
+streamlit.title('My Mom\'ss Healthy Dinner')
+
+streamlit.header('Brekfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal ')
+streamlit.text('Kale,Spinach & rocket smoothie')
+streamit.text('Hard-Boiled Free-Range Egg')
