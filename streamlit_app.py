caf721a5c3eec9ec5ba0b9aef4354e8d91fa5271
Hemant Ramesh Dange <104827347+hemantd27@users.noreply.github.com>
2022-05-05 23:48:50 +0530

    Update streamlit_app.py
    added code receive fruit name from user and pass as parameter to request URL.

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ my_fruit_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(my_fruit_show)
 
 streamlit.header('fruityvice Fruit Advice!')
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruit_choice = streamlit.text_input("what fruit would you like information about","kiwi")
+streamlit.write('The user entered',fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
