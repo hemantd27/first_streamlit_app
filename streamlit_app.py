7ae356f0543eec3b5cfa90e9457352a191875b1a
Hemant Ramesh Dange <104827347+hemantd27@users.noreply.github.com>
2022-05-06 16:15:32 +0530

    Update streamlit_app.py
    if else condition added as code

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,22 +17,28 @@ try:
   my_fruit_show = my_fruit_list.loc[fruits_selected]
   streamlit.dataframe(my_fruit_show)
   streamlit.header('fruityvice Fruit Advice!')
-  fruit_choice = streamlit.text_input("what fruit would you like information about","kiwi")
-  streamlit.write('The user entered',fruit_choice)
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
-  #streamlit.text(fruityvice_response.json())
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruityvice_normalized)
-  streamlit.stop()
-  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-  my_cur = my_cnx.cursor()
-  my_cur.execute("SELECT * from fruit_load_list")
-  my_data_rows = my_cur.fetchall()
-  streamlit.header("The fruit load list containts:")
-  streamlit.dataframe(my_data_rows)
-  fruit_choice = streamlit.text_input("what fruit do you like to add?","kiwi")
-  streamlit.write('The user entered',fruit_choice)
-  my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+  try:
+    fruit_choice = streamlit.text_input("what fruit would you like information about","kiwi")
+    if not fruit_choice:
+      streamlit.error("Please select a fruit to get an information")
+    else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+      #streamlit.write('The user entered',fruit_choice)
+      #streamlit.text(fruityvice_response.json())
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+      #my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      #my_cur = my_cnx.cursor()
+      #my_cur.execute("SELECT * from fruit_load_list")
+      #my_data_rows = my_cur.fetchall()
+      #streamlit.header("The fruit load list containts:")
+      #streamlit.dataframe(my_data_rows)
+      #fruit_choice = streamlit.text_input("what fruit do you like to add?","kiwi")
+      #streamlit.write('The user entered',fruit_choice)
+      #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+      
+  except URLError as e:
+    streamlit.error()    
 except:
   streamlit.text("exception found")
   
